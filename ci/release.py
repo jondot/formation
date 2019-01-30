bf1056ee31abf23cd490888807fb4ebe9cbf98f5
Dotan Nahum <jondotan@gmail.com>
2019-01-30 10:17:10 +0200

    fix build for py3

--- ci/release.py
+++ ci/release.py
@@ -2,8 +2,7 @@ import subprocess
 import re
 
 p = subprocess.check_output("poetry version", shell=True)
-print(p)
-ver = re.match(".* to (.*)\\.*$", str(p)).group(1)
+ver = re.match(".* to (.*)\\.*$", p.decode()).group(1)
 open("formation/__version__.py", "w").write('__version__ = "{}"'.format(ver))
 print("git tag v{}".format(ver))
 print(subprocess.check_call("git tag v{}".format(ver), shell=True))
