c8155c37af366509ae785e3643d14f49b968d7d3
Jacob Michael Silva <Funkmyster@users.noreply.github.com>
2023-01-25 22:49:58 -0600

    Fixed Link to Getting Started

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -37,7 +37,7 @@ class Footer extends React.Component {
           </a>
           <div>
             <h5>Docs</h5>
-            <a href={this.docUrl('docs/docs.html')}>
+            <a href={this.docUrl('docs.html')}>
               Getting Started
             </a>
             <a href={this.docUrl('examples/posting.html')}>
